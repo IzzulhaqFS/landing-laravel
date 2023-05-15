141535a7d9705bf7c272f9a155881b72c07cd06f
IzzulhaqFS <izzulhaq219@gmail.com>
2023-05-15 09:28:46 +0700

    Update Route dan Index

--- landing-laravel/routes/web.php
+++ landing-laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Link;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index', [
+        'links' => Link::all()->sortDesc()
+    ]);
 });
